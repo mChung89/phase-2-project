b96376e4b16ce5e6bc86b4e72cf282e30b3dade9
Michael Chung <mikechung89@gmail.com>
2022-03-28 16:39:03 -0400

    Patch working without ID

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,34 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function NewRecipe() {
   const defaultState = {
+    author: '',
     name: '',
     image: '',
     ingredients: [],
     directions: [],
   }
   const [formData, setFormData] = useState(defaultState)
-  const [author, setAuthor] = useState('')
+  const [ingredient, setIngredient] = useState('')
+  const [direction, setDirection] = useState('')
+  const [cookbookData, setCookBookData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/allcookbooks/cookbook1/')
+    .then(resp => resp.json())
+    .then(data => setCookBookData(data))
+  },[])
+  console.log(cookbookData)
 
   function handleChange (e) {
+    if (e.target.name === "ingredients") {
+      setIngredient(() => e.target.value)
+    } else if (e.target.name === "directions") {
+      setDirection(() => e.target.value)
+    } else {
+      const key = e.target.name
+      const value = e.target.value
+      setFormData({...formData, [key]:value})
+    }
+  }
+
+  function handleStepSubmit (e) {
+    e.preventDefault()
     const key = e.target.name
-    const value = e.target.value
-    setFormData({...formData, [key]:value})
+    if (key === "ingredients") {
+      setFormData({...formData, [key]: [...formData[key], ingredient]})
+    } else if (key === "directions") {
+      setFormData({...formData, [key]: [...formData[key], direction]})
+    }
   }
 
   function handleSubmit(e) {
     e.preventDefault()
-    fetch(`http://localhost:3001/${author}/recipes/`,{
-      method: 'POST',
+    console.log(formData)
+    fetch('http://localhost:3001/allcookbooks/cookbook1/', {
+      method: 'PATCH',
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify({...cookbookData, recipes: [...cookbookData.recipes, formData]})
     })
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
-
-  console.log(formData)
+  
   return (
     <div className="App">
       <p>NewRecipe</p>
@@ -36,21 +63,21 @@ function NewRecipe() {
         <h2>Add a recipe!</h2>
         <label htmlFor='name'>Recipe name</label>
         <input name='name' onChange={handleChange} value={formData.name}/>
-
+        <br></br>
         <label htmlFor='author-name'>Author Name</label>
-        <input name='author_name' onChange={handleChange} value={formData.author_name}/>
-
+        <input name='author' onChange={handleChange} value={formData.author}/>
+        <br></br>
         <label htmlFor="image">Image URL: </label>
         <input onChange={handleChange} value={formData.image} name="image"/>
-
+        <br></br>
         <label htmlFor="ingredients">Ingredients </label>
-        <input onChange={handleChange} value={formData.ingredients} name="ingredients" />
-        <button>Enter ingredient</button>
-
+        <input onChange={handleChange} value={ingredient} name="ingredients" />
+        <button name='ingredients' onClick={handleStepSubmit}>Enter ingredient</button>
+        <br></br>
         <label htmlFor="directions">List directions here! </label>
-        <input onChange={handleChange} value={formData.directions} name="directions" />
-        <button>Enter step</button>
-
+        <input onChange={handleChange} value={direction} name="directions" />
+        <button name='directions' onClick={handleStepSubmit}>Enter step</button>
+        <br></br>
         <input type="submit" value="Submit new recipe!" />
       </form>
       
