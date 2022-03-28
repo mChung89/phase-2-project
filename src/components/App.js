074e68de03c3bc7bdbea0586d37c6558470a3c04
Michael Chung <mikechung89@gmail.com>
2022-03-28 00:37:33 -0400

    File cleanup after initial create-app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import NavBar from './NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
