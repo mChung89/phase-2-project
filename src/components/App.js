9d6bbc16092d5f1484d61c1e79a0d680f472d7b1
Michael Chung <mikechung89@gmail.com>
2022-03-28 23:36:06 -0400

    recipe page complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
 import '../App.css';
 import NavBar from './NavBar'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import CookBookList from './CookBookList';
 import NewRecipe from './NewRecipe';
+import RecipeCards from './RecipeCards'
 
 function App() {
+  const [recipes,setRecipes] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3001/recipes')
+    .then(res => res.json())
+    .then(setRecipes)
+  },[])
+
+  function handleNewRecipe (newRecipe) {
+    setRecipes([...recipes, newRecipe])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
         <CookBookList />
-        <NewRecipe />
+        <NewRecipe handleNewRecipe={handleNewRecipe} />
+        <RecipeCards recipes={recipes} />
       </header>
     </div>
   );
 }
-
 export default App;
