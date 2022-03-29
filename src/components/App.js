96ad8d9e91dd5344ed79b13dfac4e7ee6068e7ad
Sarah Indano <sarah.indano@gmail.com>
2022-03-29 10:13:49 -0400

    accepted incoming changes for merge conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import '../App.css';
 import NavBar from './NavBar'
-<<<<<<< HEAD
-import {useState, useEffect} from 'react'
-=======
 import {useEffect, useState} from 'react'
->>>>>>> newRecipe
 import CookBookList from './CookBookList';
 import NewRecipe from './NewRecipe';
 import RecipeCards from './RecipeCards'
@@ -12,16 +8,6 @@ import RecipeCards from './RecipeCards'
 //json-server --watch sample.json -p 3001
 
 function App() {
-<<<<<<< HEAD
-
-  const [cookBooks, setCookBooks] = useState ([])
-
-  useEffect(() => {
-  fetch(`http://localhost:3001/allcookbooks`)
-  .then(resp => resp.json())
-  .then(cookBooksData => setCookBooks(cookBooksData))
-  }, [])
-=======
   const [recipes,setRecipes] = useState([])
   useEffect(() => {
     fetch('http://localhost:3001/recipes')
@@ -33,19 +19,13 @@ function App() {
     setRecipes([...recipes, newRecipe])
   }
 
->>>>>>> newRecipe
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
-<<<<<<< HEAD
-        <CookBookList cookBooks={cookBooks}/>
-        <NewRecipe />
-=======
         <CookBookList />
         <NewRecipe handleNewRecipe={handleNewRecipe} />
         <RecipeCards recipes={recipes} />
->>>>>>> newRecipe
       </header>
     </div>
   );
