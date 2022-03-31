38e454eeab8a56298df1f6af16527a1fe0fa25ba
Michael Chung <mikechung89@gmail.com>
2022-03-31 13:26:07 -0400

    css on recipe form, refactored

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,13 +1,8 @@
 import { useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import InputAdornment from "@mui/material/InputAdornment";
 import MenuItem from "@mui/material/MenuItem";
 import RecipePreview from "./RecipePreview";
+import RecipeForm from './RecipeForm'
 
 function NewRecipe({ handleNewRecipe }) {
   const defaultState = {
@@ -74,98 +69,7 @@ function NewRecipe({ handleNewRecipe }) {
 
   return (
     <Stack sx={{ m: 5 }} direction="row" justifyContent="center" spacing={3}>
-      <Paper sx={{ maxWidth: "50%" }}>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          direction="column"
-          sx={{ p: 3 }}
-        >
-          <Typography sx={{ p: 1 }} variant="h4">
-            Add a Recipe!
-          </Typography>
-          <TextField
-            fullWidth
-            name="name"
-            label="Recipe name"
-            value={formData.name}
-            onChange={handleChange}
-            sx={{ m: 1, bgcolor: "white" }}
-          />
-          <TextField
-            fullWidth
-            select
-            name="author"
-            label="Select Cookbook"
-            value={formData.author}
-            onChange={handleChange}
-            sx={{ m: 1, bgcolor: "white" }}
-          >
-            {renderAuthors}
-          </TextField>
-          <TextField
-            fullWidth
-            name="image"
-            label="Image URL"
-            maxRows={1}
-            value={formData.image}
-            onChange={handleChange}
-            sx={{ m: 1, bgcolor: "white" }}
-          />
-          <TextField
-            fullWidth
-            name="ingredients"
-            label="Ingredients"
-            maxRows={1}
-            value={ingredient}
-            onChange={handleChange}
-            sx={{ m: 1, bgcolor: "white" }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Button
-                    variant="outlined"
-                    name="ingredients"
-                    onClick={handleStepSubmit}
-                  >
-                    List Ingredient!
-                  </Button>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <TextField
-            fullWidth
-            name="directions"
-            label="Directions"
-            maxRows={1}
-            value={direction}
-            onChange={handleChange}
-            sx={{ m: 1, bgcolor: "white" }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Button
-                    variant="outlined"
-                    name="directions"
-                    onClick={handleStepSubmit}
-                  >
-                    List Directions!
-                  </Button>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <Button
-            variant="contained"
-            sx={{ m: 1 }}
-            type="submit"
-            value="Submit new recipe!"
-          >
-            Click Here to Submit New Recipe!
-          </Button>
-        </Box>
-      </Paper>
+      <RecipeForm handleSubmit={handleSubmit} formData={formData} handleStepSubmit={handleStepSubmit} handleChange={handleChange}/>
       {formData.name ||
       formData.author ||
       formData.image ||
--- src/components/RecipePreview.js
+++ src/components/RecipePreview.js
@@ -1,49 +1,72 @@
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 
 function RecipePreview({ formData }) {
   const renderedIngredients = formData.ingredients.map((each) => (
     <li>{each}</li>
-  ))
+  ));
 
-  const renderedDirections = formData.directions.map((each) => (
-    <li>{each}</li>
-  ))
+  const renderedDirections = formData.directions.map((each) => <li>{each}</li>);
+
+  const imageContainer = (
+    <Grid
+      component="img"
+      sx={{ ml: 4, mt: 3, objectFit: "contain", borderRadius: 2, width: "50%" }}
+      src={formData.image}
+    />
+  );
+
+  const ingredientsGrid = (
+    <Grid
+      sx={{
+        width: "50%",
+        ml: 2,
+        mt: 3,
+        mr: 4,
+        border: 1,
+        borderRadius: 2,
+        maxHeight: "100%",
+        overflow: "auto",
+      }}
+    >
+      <Typography sx={{ p: 1 }}>
+        Ingredients :<ul>{renderedIngredients}</ul>
+      </Typography>
+    </Grid>
+  );
+
+  const directionsGrid = (
+    <Grid
+      item
+      sx={{
+        m: 2,
+        ml: 4,
+        mr: 4,
+        border: 1,
+        borderRadius: 2,
+        maxHeight: "55%",
+        overflow: "auto",
+      }}
+    >
+      <Typography sx={{ p: 1 }}>
+        Directions:
+        <ol>{renderedDirections}</ol>
+      </Typography>
+    </Grid>
+  );
+
   return (
-    <Paper sx={{ bgcolor: "white", minWidth: "50%", height: 600 }}>
-      <Container
-        component="img"
-        sx={{ maxHeight: "40%", mt: 3}}
-        src={formData.image}
-      />
-      <Container direction="column" sx={{ m: 2 }}>
-        <Typography variant="h5">
-          {formData.name ? `Recipe Name: ${formData.name}` : null}
-        </Typography>
-        <Typography>
-          {formData.author ? `Recipe by: ${formData.author}` : null}
-        </Typography>
-      </Container>
-      <Grid container direction="row" sx={{ p: 3 }}>
-        <Grid xs={6} item>
-          <Typography>
-            {formData.ingredients[0] !== undefined ? `Ingredients :` : null}
-            <ul>
-              {renderedIngredients}
-            </ul>
-          </Typography>
-        </Grid>
-        <Grid xs={6} item>
-          <Typography>
-            {formData.directions[0] !== undefined ? `Directions: ` : null}
-            <ol>
-              {renderedDirections}
-            </ol>
-          </Typography>
-        </Grid>
-      </Grid>
+    <Paper
+      elevation={3}
+      sx={{ bgcolor: "white", minWidth: "50%", height: 600 }}
+    >
+      <Stack direction="row" sx={{ height: "40%" }}>
+        {formData.image ? imageContainer : null}
+        {formData.ingredients[0] !== undefined ? ingredientsGrid : null}
+      </Stack>
+      {formData.directions[0] !== undefined ? directionsGrid : null}
     </Paper>
   );
 }
